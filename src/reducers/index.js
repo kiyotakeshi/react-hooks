7494daa0cd47d3d379385f6f16b5f920bbc1ae4a
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-04-28 08:21:26 +0900

    create event reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+const events = (state = [], action) => {
+    switch (action.type) {
+        case 'CREATE_EVENT':
+            const event = { title: action.title, body: action.body };
+            const length = state.length;
+            const id = length === 0 ? 1 : this.state[length - 1].id + 1;
+            // id: id なのでショートハンドで書ける
+            return [...state, { id, ...event }];
+        // TODO:
+        case 'DELETE_EVENT':
+            return state;
+        case 'DELETE_ALL_EVENTS':
+            return [];
+        default:
+            return state;
+    }
+};
