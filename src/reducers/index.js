f2f67c277187cf89d0a59adaeca0f1f86cae4dc2
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-05-05 13:00:01 +0900

    implement delete action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,15 @@
 const events = (state = [], action) => {
     switch (action.type) {
         case 'CREATE_EVENT':
-            console.log('Reducer create event');
+            // console.log('Reducer create event');
             const event = { title: action.title, body: action.body };
             const length = state.length;
-            const id = length === 0 ? 1 : this.state[length - 1].id + 1;
+            const id = length === 0 ? 1 : state[length - 1].id + 1;
             // id: id なのでショートハンドで書ける
             return [...state, { id, ...event }];
-        // TODO:
         case 'DELETE_EVENT':
-            return state;
+            // action で渡ってくる id は削除対象のため、等しくないものだけ抽出
+            return state.filter((event) => event.id !== action.id);
         case 'DELETE_ALL_EVENTS':
             return [];
         default:
